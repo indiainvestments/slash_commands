b2da87f461d3666f8d871f2bb9e50e95368117b4
Berlin <berlinuwuart@protonmail.com>
2021-05-01 17:51:39 +0530

    initial commit

---
+++ gitbook_client.ts
@@ -0,0 +1,146 @@
+interface GitbookClientOptions {
+  version?: "v1";
+  spaceId: string;
+  gitbookUrl: string;
+}
+
+interface GitbookSearchSection {
+  key: string;
+  title: string;
+  content: string;
+  url: string;
+}
+
+interface GitbookSearchNode {
+  id: string;
+  title: string;
+  sections: GitbookSearchSection[];
+  url: string;
+  path: string;
+}
+
+interface GitbookSpace {
+  uid: string;
+  name: string;
+  baseName: string;
+  private: boolean;
+  unlisted: boolean;
+}
+
+interface GitbookVariant {
+  uid: string;
+  ref: string;
+  title: string;
+  page: GitbookPage;
+}
+
+interface GitbookAsset {
+  uid: string;
+  name: string;
+  downloadURL: string;
+  contentType: "image/png" | `${string}/${string}`;
+}
+
+interface GitbookPage {
+  uid: string;
+  title: string;
+  description: string;
+  kind: "document" | "group";
+  path: string;
+  pages: GitbookPage[] | never;
+}
+
+interface GitbookContent {
+  uid: string;
+  parents: string[];
+  variants: GitbookVariant[];
+  assets: GitbookAsset[];
+}
+
+export class GitbookSpaceClient {
+  public gitbookUrl: string;
+  public ApiUrl: string;
+  public spaceId: string;
+  public headers: Headers;
+  public version: string;
+  constructor(
+    token: string,
+    { spaceId, gitbookUrl, version }: GitbookClientOptions,
+  ) {
+    this.ApiUrl = "https://api-beta.gitbook.com";
+    this.spaceId = spaceId;
+    // trim forward slash
+    this.gitbookUrl = gitbookUrl.endsWith("/")
+      ? gitbookUrl.slice(0, -1)
+      : gitbookUrl;
+    this.version = version ?? "v1";
+    const headers = new Headers();
+    headers.set("Authorization", `Bearer ${token}`);
+    this.headers = headers;
+  }
+
+  getSpaceUrl(path = "/", params?: URLSearchParams) {
+    // trim beginning slash
+    const rest = path.startsWith("/") ? path.slice(1, path.length) : path;
+    const url = new URL(
+      `${this.version}/spaces/${this.spaceId}/${rest}`,
+      this.ApiUrl,
+    );
+    if (params) {
+      url.search = params.toString();
+    }
+    return url;
+  }
+
+  async get(path = "/", params?: URLSearchParams) {
+    const url = this.getSpaceUrl(path, params);
+    const response = await fetch(url, { headers: this.headers });
+    return response.json();
+  }
+
+  getSpace(): Promise<GitbookSpace> {
+    return this.get();
+  }
+
+  getRoot(): Promise<GitbookContent> {
+    return this.get("content");
+  }
+
+  async searchSpace(
+    query: string,
+  ) {
+    const { results } = await this.get(
+      "search",
+      new URLSearchParams({ query }),
+    ) as { results: GitbookSearchNode[] };
+    return results.map((item) => {
+      return {
+        ...item,
+        url: `${this.gitbookUrl}/${item.url}`,
+        path: item.url
+      };
+    });
+  }
+
+  async list(query: string, variant = "main") {
+    const [main] = await this.searchSpace(query);
+    if (!main) {
+      throw new Error(`No results found for query: ${query}`);
+    }
+    const content: GitbookPage = await this.get(
+      `content/v/${variant}/url/${main.path}`,
+    );
+    return {
+      title: content.title,
+      description: content.description,
+      url: `${this.gitbookUrl}/${main.url}`,
+      items: (content.pages ?? []).map((page) => {
+        return {
+          title: page.title,
+          description: page.description,
+          url: `${this.gitbookUrl}/${main.url}/${page.path}`,
+        };
+      }),
+    };
+  }
+}
---
+++ gitbook_commands.ts
@@ -0,0 +1,120 @@
+import * as slash from "https://raw.githubusercontent.com/harmonyland/harmony/main/deploy.ts";
+import { Embed } from "https://raw.githubusercontent.com/harmonyland/harmony/main/src/structures/embed.ts";
+import { chunk, randomHexColorGen } from "./utils.ts";
+import { GitbookSpaceClient } from "./gitbook_client.ts";
+
+const { env } = Deno;
+
+slash.init({
+  env: true,
+});
+
+const COMMANDS_SIZE = 2;
+const GITBOOK_SPACE_ID = env.get("GITBOOK_SPACE_ID")!;
+const GITBOOK_TOKEN = env.get("GITBOOK_TOKEN")!;
+const GITBOOK_URL = env.get("GITBOOK_URL")!;
+const GITBOOK_NAME = env.get("GITBOOK_NAME")!;
+
+const randomHexColor = randomHexColorGen();
+
+const client = new GitbookSpaceClient(GITBOOK_TOKEN, {
+  spaceId: GITBOOK_SPACE_ID,
+  gitbookUrl: GITBOOK_URL,
+});
+
+const commands = await slash.commands.all();
+
+// Create Slash Commands
+if (commands.size !== COMMANDS_SIZE) {
+  slash.commands.bulkEdit([
+    {
+      name: "list",
+      description: `List links from a page or group of ${GITBOOK_NAME} wiki.`,
+      options: [{
+        name: "query",
+        description: "Query to search.",
+        type: slash.SlashCommandOptionType.STRING,
+        required: true,
+      }],
+    },
+    {
+      name: "search",
+      description: `Search ${GITBOOK_NAME} wiki.`,
+      options: [{
+        name: "query",
+        description: "Query to search.",
+        type: slash.SlashCommandOptionType.STRING,
+        required: true,
+      }, {
+        name: "limit",
+        description: "Limit number of results. By default, it only shows 1.",
+        type: slash.SlashCommandOptionType.INTEGER,
+      }],
+    },
+  ]);
+}
+
+slash.handle("search", async (interaction) => {
+  const [query, limit] = interaction.options;
+  const results = await client.searchSpace(query.value);
+  if (!results.length) {
+    return interaction.reply({
+      content: `Nothing found for your query: \`${query.value}\``,
+      ephemeral: true,
+    });
+  }
+
+  if (limit) {
+    return interaction.reply(
+      results.slice(0, limit.value).map((item) => item.url).join("\n"),
+    );
+  }
+  return interaction.reply(results[0].url);
+});
+
+slash.handle("list", async (interaction) => {
+  const [query] = interaction.options;
+  try {
+    const result = await client.list(query.value);
+    const color = randomHexColor.next().value;
+    const embeds: Embed[] = [];
+
+    const header = new Embed()
+      .setTitle(result.title)
+      .setURL(result.url)
+      .setColor(color);
+
+    if (result.description) {
+      header.setDescription(result.description);
+    }
+
+    embeds.push(header);
+
+    const chunked = chunk(result.items, 5);
+
+    for (const c of chunked) {
+      const desc = c.map((item) => {
+        return `**[${item.title}](${item.url})**\n${item.description ||
+          "No description available."}`;
+      }).join("\n\n");
+
+      const em = new Embed()
+        .setDescription(desc)
+        .setColor(color);
+
+      embeds.push(em);
+    }
+
+    return interaction.respond({
+      embeds,
+    });
+  } catch (err) {
+    interaction.reply({
+      content: err.message,
+      ephemeral: true,
+    });
+  }
+});
+
+slash.handle("*", (d) => d.reply("Unhandled Command", { ephemeral: true }));
+slash.client.on("interactionError", console.error);
