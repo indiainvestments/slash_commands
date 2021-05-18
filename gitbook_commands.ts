7cbeb6f655fd2d92697ab74ab16b900f0ad929dc
Dwight Schrute <84026436+dwight-schrute@users.noreply.github.com>
2021-05-18 10:04:57 +0530

    fix: changed command name to wiki and handling

--- gitbook_commands.ts
+++ gitbook_commands.ts
@@ -1,6 +1,6 @@
 import * as slash from "https://raw.githubusercontent.com/indiainvestments/harmony/main/deploy.ts";
 import { Embed } from "https://raw.githubusercontent.com/harmonyland/harmony/ce455c50c3af667a02077db5ffb79c5086510945/src/structures/embed.ts";
-import { chunk, randomHexColorGen } from "./utils.ts";
+import { randomHexColorGen } from "./utils.ts";
 import { GitbookSpaceClient } from "./gitbook_client.ts";
 import { EmbedAuthor } from "https://raw.githubusercontent.com/indiainvestments/harmony/main/deploy.ts";
 import { GitbookSearchNode } from "./types/index.d.ts";
@@ -11,11 +11,9 @@ slash.init({
   env: true,
 });
 
-const COMMANDS_SIZE = 2;
 const GITBOOK_SPACE_ID = env.get("GITBOOK_SPACE_ID") ?? '';
 const GITBOOK_TOKEN = env.get("GITBOOK_TOKEN") ?? '';
 const GITBOOK_API_URL = env.get("GITBOOK_API_URL") ?? '';
-const GITBOOK_NAME = env.get("GITBOOK_NAME");
 
 const randomHexColor = randomHexColorGen();
 
@@ -26,45 +24,11 @@ const client = new GitbookSpaceClient(GITBOOK_TOKEN, {
 
 const cache = new Cache(client);
 await cache.fillData();
-const commands = await slash.commands.all();
-// Create Slash Commands
-if (commands.size !== COMMANDS_SIZE) {
-  slash.commands.bulkEdit([
-    {
-      name: "list",
-      description: `List links from a page or group of ${GITBOOK_NAME} wiki.`,
-      options: [
-        {
-          name: "query",
-          description: "Query to search.",
-          type: slash.SlashCommandOptionType.STRING,
-          required: true,
-        },
-      ],
-    },
-    {
-      name: "weighted",
-      description: `Search ${GITBOOK_NAME} wiki.`,
-      options: [
-        {
-          name: "query",
-          description: "Query to search.",
-          type: slash.SlashCommandOptionType.STRING,
-          required: true,
-        },
-        {
-          name: "limit",
-          description: "Limit number of results. By default, it only shows 1.",
-          type: slash.SlashCommandOptionType.INTEGER,
-        },
-      ],
-    },
-  ]);
-}
 
-slash.registerHandler("weighted", async (interaction) => {
-  const [query, limit = {value: 1}] = interaction.options;
+slash.registerHandler("wiki", async (interaction) => {
+  const [query] = interaction.options;
   try {
+    console.log("wiki search", query);
     let {results, timeTaken} = await client.searchSpace(query.value);
     if (!results.length) {
       return interaction.reply({
@@ -72,23 +36,29 @@ slash.registerHandler("weighted", async (interaction) => {
         ephemeral: true,
       });
     }
-    results = results.slice(0, limit.value);
+    const resultsSize = results.length;
+    results = results.slice(0, 5);
 
     const embeds = [];
     const color = randomHexColor.next().value;
-    const contentChunks = chunk(results, 5);
+    // const contentChunks = chunk(results, 5);
 
-    for (const contentChunk of contentChunks) {
-      const desc = contentChunk.map((content: GitbookSearchNode) => {
-        const description = cache.getValue(content.uid);
-        return `**[${content.title}](${client.iiGitbookBaseUrl}/${content.path})**\n${
-          (description && description !== "") ? description : "No description available."
-        }`
-      }).join("\n\n");
-      const embed = new Embed().setColor(color);
-      embed.setDescription(desc);
-      embeds.push(embed);
+    // for (const contentChunk of contentChunks) {
+    let desc = results.map((content: GitbookSearchNode) => {
+      const description = cache.getValue(content.uid);
+      return `**[${content.title}](${client.iiGitbookBaseUrl}/${content.path})**\n${
+        (description && description !== "") ? description : "No description available."
+      }`
+    }).join("\n\n");
+    console.log("5 desc", desc);
+    if (resultsSize > 5) {
+      desc = `${desc}\n\n[click here more results](${client.iiGitbookBaseUrl}/?q=${encodeURI(query.value)})`;
+      console.log("> 5 desc", desc);
     }
+    const embed = new Embed().setColor(color);
+    embed.setDescription(desc);
+    embeds.push(embed);
+    // }
 
     if (embeds.length <= 0) {
       return interaction.reply({
@@ -101,7 +71,7 @@ slash.registerHandler("weighted", async (interaction) => {
       icon_url: interaction.user.avatarURL()
     }
     embeds[0].setAuthor(author);
-    embeds[embeds.length - 1].setFooter(`\/weighted query: ${query.value} limit: ${limit.value} | retrieved in ${(timeTaken).toString().padEnd(3, '0')} seconds`);
+    embeds[embeds.length - 1].setFooter(`\/wiki query: ${query.value} | retrieved in ${(timeTaken).toString().padEnd(3, '0')} seconds`);
     return interaction.respond({
       embeds,
     });
