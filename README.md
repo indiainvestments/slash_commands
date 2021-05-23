d1668fb010a3f39e7033ba47b4fed43e950976de
crimelabs786 <56079307+crimelabs786@users.noreply.github.com>
2021-05-23 09:08:01 +0530

    add commits from pull request #13

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+![GitHub Action](https://github.com/indiainvestments/slash_commands/actions/workflows/deno-lint-tests.yml)
+
 # Introduction
 
 IndiaInvestments is a community to discuss investments, insurance, finance, economy, and markets in India. This website is a collection of advice and information we have organized as a community.
@@ -6,11 +8,10 @@ If you want to discuss anything on this website with other people, please check 
 
 [![](https://img.shields.io/reddit/subreddit-subscribers/indiainvestments?style=social)](https://reddit.com/r/indiainvestments) [![](https://img.shields.io/discord/546638391127572500)](https://discord.gg/hqBNg4u) [![All Contributors](https://img.shields.io/badge/all_contributors-20-orange.svg?style=flat-square)](./#contributors)
 
-
 # Searching Wiki Bot
-Slash commands for india investment Discord server to search the wiki. 
+
+Slash commands for india investment Discord server to search the wiki.
 
 # Setting Up & Running the Code
 
 [Installation & Setting Up](https://github.com/indiainvestments/slash_commands/wiki/Installation-&-Setting-Up)
-
---
+++ test/__mocks__/gitbook_content_mock.js
@@ -0,0 +1,310 @@
+export const mock1 = {
+  uid: "-M_uhYUHzC_XF8GWC2Li",
+  parents: ["-M_ugiSwD2UFTdTC7mhN"],
+  variants: [
+    {
+      uid: "main",
+      ref: "main",
+      title: "main",
+      page: {
+        uid: "-MUEnNPwNxmH8dSZmQfX",
+        title: "Introduction",
+        description: "test description to be used in cache test initially",
+        kind: "document",
+        path: "undefined",
+        pages: [
+          {
+            uid: "-MVk5ZMXrwmANrFFxtgG",
+            title: "Disclaimers and Disclosures",
+            description:
+              "Disclaimers and disclosures that you should read and understand before reading this wiki",
+            kind: "document",
+            path: "disclaimers-and-disclosures",
+            pages: [],
+          },
+          {
+            uid: "-MUEng9FNaSKG8eTu-4y",
+            title: "FAQs",
+            description: "",
+            kind: "group",
+            path: "faqs",
+            pages: [
+              {
+                uid: "-MXQ8dL8Dq4GyMy_iLXV",
+                title: "Mutual Funds & ETFs",
+                description: "FAQs on all things mutual funds",
+                kind: "document",
+                path: "mfs",
+                pages: [
+                  {
+                    uid: "-MV_jst92u0bxk6We-dX",
+                    title: "What is the best mutual fund app for investments?",
+                    description:
+                      "Pick one you're comfortable with, as long as the platform allows buying direct plan, growth scheme; free of cost. Beyond this, your specific choice of app won't affect your returns.",
+                    kind: "document",
+                    path: "best-mutual-fund-app-for-investments",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MXuKAZ-A_2IQ8Ivnbrx",
+                    title:
+                      "Why should I invest in Direct Plans instead of Regular Plans?",
+                    description:
+                      "Direct plans of mutual funds have no commission, lower fees compared to its Regular plan counterpart. Returns are higher with no extra risk.",
+                    kind: "document",
+                    path: "direct-vs-regular",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MUEng9JC7piIzOmciC_",
+                    title: "What’s the best mutual fund I can invest in?",
+                    description:
+                      "No one can predict how a mutual fund would perform in the long run. Data also shows consistently chasing best mutual funds result in behavior gap. Pick one that you can stay with for long term.",
+                    kind: "document",
+                    path: "whats-the-best-mutual-fund-i-can-invest-in",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MUEng9Kf1mbbNtD8DQr",
+                    title: "Which date(s) is/are best for SIP in a month?",
+                    description:
+                      "There's no such thing. It's rare for markets to move up / down so much in a single month, that a single SIP instalment being on a different date would make a sizeable difference in your long term.",
+                    kind: "document",
+                    path: "which-date-s-is-are-best-for-sip-in-a-month",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MUEng9OcrG-4adUaSnm",
+                    title:
+                      "I’ve to invest in ELSS for 80C tax saving. Which fund(s) should I pick?",
+                    description:
+                      "Pick a fund that you like. No point having more than one ELSS fund. But know why you're investing in ELSS in the first place, because it's equity, and you should give it longer than 3 years to perform",
+                    kind: "document",
+                    path: "ive-to-invest-in-elss-for-80c-tax-saving.-which-fund-s-should-i-pick",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MUEng9MY2PvJslboPT3",
+                    title:
+                      "Should I get a Demat Account to buy units in Mutual Funds?",
+                    description:
+                      "No! You do not need demat account / trading account, to invest in mutual funds.",
+                    kind: "document",
+                    path: "should-i-get-a-demat-account-to-buy-units-in-mutual-funds",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MUEng9UgulJUDPHdobW",
+                    title: "Lumpsum investment vs SIP/DCA",
+                    description:
+                      "If you're investing for the long term, time in the market beats timing the market. Invest the entire amount all at once, irrespective of market levels. Invest regularly if don't have lumpsum.",
+                    kind: "document",
+                    path: "lumpsum-investment-vs-sip-dca",
+                    pages: [],
+                  },
+                ],
+              },
+              {
+                uid: "-MXQ9GOqaXO0-raFLx_7",
+                title: "Insurance",
+                description: "All frequently asked queries on insurance",
+                kind: "document",
+                path: "insurance",
+                pages: [
+                  {
+                    uid: "-MUEng9PgdNorvjagdty",
+                    title: "Should I invest in this LIC policy?",
+                    description:
+                      "Avoid. LIC and the agent would take most of it. You'd be left with peanuts. It's one of the most opaque form of investments, and there's no guarantee. Govt. backs LIC, not your financial future.",
+                    kind: "document",
+                    path: "should-i-invest-in-this-lic-policy",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MUEng9QLqim6YDfrIeT",
+                    title:
+                      "Opinions on investing in smart wealth plan by bank?",
+                    description:
+                      "Avoid. Insurance company and bank RM would make gains; but you could do much better not mixing insurance with investments. Insurance cover is inadequate, returns from ULIPs are lower than bank deposit",
+                    kind: "document",
+                    path: "opinions-on-investing-in-smart-wealth-plan-by-bank",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MUEng9XPirOS7Q80-xD",
+                    title: "Up to what age should I take term cover?",
+                    description:
+                      "Till the age someone is financially dependent on your income. Ideally 60-65, no more.",
+                    kind: "document",
+                    path: "up-to-what-age-should-i-take-term-cover",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MVWEgx-ouI3y1UO4ZfJ",
+                    title:
+                      "Do I need my own health insurance? Employer already has group policy",
+                    description:
+                      "Yes, you should. Employer provided group policy might have lower cover, or co-pay, or a cap on room-rent. You should get one on your own, to decouple your insurance from your employer",
+                    kind: "document",
+                    path: "do-i-need-my-own-health-insurance-employer-already-has-group-policy",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MXQDDdU-GuvvpoUdf54",
+                    title: "Should I take top-up policy or super top-up?",
+                    description:
+                      "Super top-up is better, as the limit is reset on every claim. While for top-up, it's per policy year",
+                    kind: "document",
+                    path: "top-up-vs-super-top-up",
+                    pages: [],
+                  },
+                ],
+              },
+              {
+                uid: "-MXQAJHEeGFTSFp84E6N",
+                title: "Stocks",
+                description: "FAQs on direct equity investments",
+                kind: "document",
+                path: "stocks",
+                pages: [
+                  {
+                    uid: "-MUEng9SPxOCHirMmXwS",
+                    title: "Should I invest in smallcase?",
+                    description:
+                      "Smallcase has tools which can help managing a stock portfolio better than most platforms. But do your own due diligence, before you invest in a smallcase; and not just because the CAGR looks great.",
+                    kind: "document",
+                    path: "is-smallcase-a-good-investment-returns-look-good",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MWE7cUcWNEAicy0uJHg",
+                    title: "What is the best app for buying or trading stocks?",
+                    description:
+                      "Pick a discount broker you're comfortable with, as per your investment style and functionality requirements",
+                    kind: "document",
+                    path: "what-is-the-best-app-for-buying-or-trading-stocks",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MXzOeZkLBjp8R4Cn_zs",
+                    title: "Which screener(s) should I use?",
+                    description:
+                      "Pick a screener which doesn't bias you for or against a stock. But you should compute the ratios yourself, from ARs, to understand the assumptions behind those computations.",
+                    kind: "document",
+                    path: "which-screener",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MZYyeOewwWzf2bGzSZZ",
+                    title:
+                      "The Stock Market Has Crashed. Which Stocks Should I Buy?",
+                    description:
+                      "Corrections in the stock market aren't unusual. Do your own due diligence before buying stocks.",
+                    kind: "document",
+                    path: "stock-market-crash-which-stocks-should-i-buy",
+                    pages: [],
+                  },
+                ],
+              },
+              {
+                uid: "-MXQAjlPP7Wex8lkg28h",
+                title: "Foreign Investing",
+                description: "FAQs on Investing in the US and overseas markets",
+                kind: "document",
+                path: "foreign-invest",
+                pages: [
+                  {
+                    uid: "-MWiq7HnAI0TndJLvZ9G",
+                    title: "Why should I invest in the US markets?",
+                    description:
+                      "US equities give diversification and exposure to global growth in the most stable currency. Invest in US equities, if you can find a cost-effective way to do it.",
+                    kind: "document",
+                    path: "why-should-i-invest-in-the-us-markets",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MWj0Wjz9BIcwSsWRe98",
+                    title: "How should I invest in US equity?",
+                    description:
+                      "Invest in a cost-effective manner. For smaller corpus, it makes sense to invest via India-domiciled mutual funds that invest overseas, while with a larger corpus it could be cheaper to invest directly",
+                    kind: "document",
+                    path: "how-should-i-invest-in-us-equity",
+                    pages: [],
+                  },
+                ],
+              },
+              {
+                uid: "-MXQAxxTnYwEGsDL1wUl",
+                title: "Tax",
+                description: "Frequently asked queries around taxes",
+                kind: "document",
+                path: "tax",
+                pages: [
+                  {
+                    uid: "-MUEng9TFw3HPDM1K9Ze",
+                    title:
+                      "I don't have any tax to pay. Do I still have to file ITR?",
+                    description:
+                      "File a return, it is an important piece of paperwork that can come in handy later. Even if you have no tax to pay. It would take 5-10 mins at worst.",
+                    kind: "document",
+                    path: "i-dont-have-any-tax-to-pay.-do-i-still-have-to-file-itr",
+                    pages: [],
+                  },
+                ],
+              },
+              {
+                uid: "-MXQ9k7wuPwJIRQ6pYB0",
+                title: "Miscellaneous",
+                description:
+                  "Various frequently asked queries, all in one place",
+                kind: "document",
+                path: "misc",
+                pages: [
+                  {
+                    uid: "-MUEng9VuXc0R-tEJLsx",
+                    title:
+                      "Where can I park money for a few days, a few months, or a few years?",
+                    description:
+                      "For shorter durations, few days to few months; Overnight funds / Liquid funds / Money Market funds are alright. High Yield Savings Account (HYSA) are ok. If large amount, consider registered advisor.",
+                    kind: "document",
+                    path: "where-can-i-park-money-for-a-few-days-a-few-months-or-a-few-years",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MUEng9NXuhqkQEQtN09",
+                    title: "What are chit funds? Should I invest?",
+                    description:
+                      "Closed system, where someone's gain is another person's loss. Does not scale with corpus size. Has little to no regulatory oversight. Stay away. Also called as chit, kitty, chitty, kuri, etc.",
+                    kind: "document",
+                    path: "explain-like-i-am-5-eli5-chit-funds",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MUEng9RJCHPXGoK6W-5",
+                    title:
+                      "Is Gold a good investment now? It has gone up ~50% this year",
+                    description:
+                      "If that is indeed true, best time to buy gold was a year ago. It can be a great investment, but one cannot judge good or bad investment only the basis of recent returns.",
+                    kind: "document",
+                    path: "is-gold-a-good-investment-now-it-has-gone-up-50-this-year",
+                    pages: [],
+                  },
+                  {
+                    uid: "-MUEng9WsugRQ0zfmy1M",
+                    title:
+                      "I cannot send messages to stocks-fundamentals channel on Discord. Why?",
+                    description:
+                      "This is done to cull low-effort speculative queries on stocks. Share your thesis in the popular channels. You'd be able to earn the role to post in that channel.",
+                    kind: "document",
+                    path: "i-cannot-send-messages-to-stocks-fundamentals-channel-on-discord.-why",
+                    pages: [],
+                  },
+                ],
+              },
+            ],
+          },
+        ],
+      },
+    },
+  ],
+};
